11f28c7ba0b5b07dfd89db427d72a9d46c243539
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2024-02-19 23:37:59 +0300

    Реструктуризация

---
+++ 03. Очередь событий/Типы событий.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding:utf-8
+import pygame as pg
+import pygame.locals as lc
+
+
+size = width, height = 800, 600
+pg.init()
+scr = pg.display.set_mode(size)
+end_game = False
+
+while not end_game:
+    for event in pg.event.get():
+        if event.type == lc.QUIT:
+            end_game = True
+        elif event.type == lc.MOUSEMOTION:
+            pg.display.set_caption(f"pos:{event.pos}, rel:{event.rel}")
+            continue
+        print(f"{event}")
+
+pg.quit()

f3e81a3aece50a41996968dbd2c0b1b0d9041dff
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2024-03-30 21:46:31 +0300

    Реструктуризация 4

--- 06. Классы. Объекты/Base_Class.py
+++ 06. Классы. Объекты/Base_Class.py
@@ -11,9 +11,12 @@ class PGObject:
         self.rect = self.image.get_rect()
 
     def create(self):
-        scr = pg.Surface((200, 200), pg.SRCALPHA, 32)
-        scr = scr.convert_alpha()
-        pg.draw.circle(scr, 'white', (100, 100), self.radius)
+        # scr = pg.Surface((200, 200), pg.SRCALPHA, 32)
+        # scr = scr.convert_alpha()
+        # pg.draw.circle(scr, 'white', (100, 100), self.radius)
+        scr = pg.image.load('bird.png')
+        scr.convert()
+        scr.set_colorkey((150, 150, 150))
         return scr
 
     def draw(self, screen: pg.Surface):
@@ -22,9 +25,9 @@ class PGObject:
         screen.blit(self.image, (x - width // 2, y - height // 2))
 
     def move(self, screen: pg.Surface, dx=0, dy=0):
-        self.radius += 1
-        self.image = self.create()
-        self.rect = self.image.get_rect()
+        # self.radius += 1
+        # self.image = self.create()
+        # self.rect = self.image.get_rect()
         x, y = self.place
         width, height = screen.get_size()
         if 0 < x < width and 0 < y < height:
